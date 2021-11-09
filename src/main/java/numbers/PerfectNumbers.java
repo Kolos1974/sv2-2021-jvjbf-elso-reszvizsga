496c6f26f124b9487cd85f07efc914011ef2299f
Kolos1974 <karlovitz.kolos@gmail.com>
2021-11-09 15:09:37 +0100

    refactor perfectnumbers task

--- src/main/java/numbers/PerfectNumbers.java
+++ src/main/java/numbers/PerfectNumbers.java
@@ -7,11 +7,7 @@ public class PerfectNumbers {
 
     public boolean isPerfectNumber(int number){
         List<Integer> divider = new ArrayList<>();
-        for (int i=1; i<number; i++){
-            if (number%i==0){
-                divider.add(i);
-            }
-        }
+        fillDividerList(number, divider);
 
         int sum = 0;
         for (int element: divider){
@@ -22,5 +18,13 @@ public class PerfectNumbers {
             return true;
         }
         return false;
+    }
+
+    private void fillDividerList(int number, List<Integer> divider) {
+        for (int i = 1; i< number; i++){
+            if (number %i==0){
+                divider.add(i);
+            }
+        }
     }
 }
